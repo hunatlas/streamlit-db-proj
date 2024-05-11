b3f7a5ea982ff3a32425b7ab26ff9c191089632e
Kiss Gabor <hun.atlas@gmail.com>
2024-05-11 19:00:06 +0200

    Fix connection issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import json
 
 key_dict = json.loads(st.secrets['textkey'])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project='database 24')
+db = firestore.Client(credentials=creds)
 
 doc_ref = db.collection('books').document('Hyperion')
 doc = doc_ref.get()

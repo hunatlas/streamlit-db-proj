67b05977a2d1ba073f0be428e345fcffad5e3ec2
Kiss Gabor <hun.atlas@gmail.com>
2024-05-11 19:08:38 +0200

    Fix connection issues again

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,11 @@ from google.cloud import firestore
 from  google.oauth2 import service_account
 import json
 
-key_dict = json.loads(st.secrets['textkey'])
-creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project='database-24-415313')
+#key_dict = json.loads(st.secrets['textkey'])
+#creds = service_account.Credentials.from_service_account_info(key_dict)
+#db = firestore.Client(credentials=creds, project='database-24-415313')
+
+db = firestore.Client.from_service_account_json('database-24-firestore-key.json')
 
 doc_ref = db.collection('books').document('Hyperion')
 doc = doc_ref.get()

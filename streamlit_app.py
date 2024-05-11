172d48ccff9657d984361cb4aabaf247a70cb02b
Kiss Gabor <hun.atlas@gmail.com>
2024-05-11 18:57:30 +0200

    Creating key for Firestore connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
+from google.cloud import firestore
+from  google.oauth2 import service_account
+import json
 
-st.header('Hello world')
-if st.button('Balloons'):
-    st.balloons()
+key_dict = json.loads(st.secrets['textkey'])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project='database 24')
+
+doc_ref = db.collection('books').document('Hyperion')
+doc = doc_ref.get()
+
+st.write(f'Book id: {doc.id}')
+st.write(f'Book information: {doc.to_dict()}')
